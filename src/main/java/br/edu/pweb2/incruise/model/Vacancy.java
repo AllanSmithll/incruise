b06d0b4da1636a31ed3ad1bab11d5474bd3c9919
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2024-04-06 00:20:23 -0300

    criação da pagina info de company
    html e css

--- src/main/java/br/edu/pweb2/incruise/model/Vacancy.java
+++ src/main/java/br/edu/pweb2/incruise/model/Vacancy.java
@@ -1,8 +1,6 @@
 package br.edu.pweb2.incruise.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
